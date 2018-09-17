36fb029cd468b72273c7745fc15f12818b192b54
Jesper <jesper@Jespers-MacBook-Pro.local>
2018-09-17 11:24:02 +0200

    Added a first test client

---
+++ server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#TSCP_server script
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 9999
+
+s.bind((host,port))
+
+print "Waiting for connection..."
+s.listen(5)
+
+while True:
+    conn,addr = s.accept()
+    print 'Got connection from ', addr
+    conn.send('Server saying Hi')
+    conn.close()
