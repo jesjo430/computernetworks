b9efaec34fca19d3bec5c51d08f15d345f5c9254
Jesper <jesper@c-10-253-208-18.clients.eduroam.liu.se>
2018-09-20 14:04:12 +0200

    Even new imp

--- proxy.py
+++ proxy.py
@@ -5,79 +5,97 @@ from server import Server
 from thread import start_new_thread
 import sys, socket
 
-MAX_CONNECTIONS = 10 #Max buffer in connection
+MAX_CONNECTIONS = 5 #Max buffer in connection
 BUFFER_SIZE = 2048 #Amount of data to handle in chunks
-HOST = ('127.0.0.1',8001) #Temp 
+PROXY_HOST = ('127.0.0.1',8001) #Temp
+DEBUG = True
 
 try:
     listen_port = 8001
     #int(raw_input("Enter port number: ")) #User input for chosing port
 except KeyboardInterrupt:
-    print "\nShutting down..."
+    print "\n[*] Shutting down..."
     sys.exit()
 
 def start():
     #************************** INIT ****************************#
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Init socket as TCP
-        s.bind(HOST) #Bind port to listen at
-        s.listen(MAX_CONNECTIONS) #Start listen 
-        print "Server listening at " + str(HOST)
+        if DEBUG: print "Creating socket for proxy server...\n"
+        conn_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Init socket as TCP
+        if DEBUG: print "Binding socket...\n"
+        conn_server.bind(PROXY_HOST) #Bind port to listen at
+        conn_server.listen(MAX_CONNECTIONS) #Start listen 
+        print "Proxy listening at " + str(PROXY_HOST)
     except Exception, e:
-        print "Failed to start before setup:"
+        print "[*] Failed to start before setup"
         print e.message
         sys.exit(1)
 
     #***************** Waiting for client req. *******************#
     while 1:
         try:
-            conn_client, addr = s.accept() #Accept connection from workstation
-            data = conn_client.recv(BUFFER_SIZE) # Recieve data from user
-            start_new_thread(conn_thread, (conn_client, data, addr)) #Start new thread to handle request from user
+            if DEBUG: print "\nWaiting for request...\n"
+            conn_client, addr = conn_server.accept() #Accept connection from workstation
+            if DEBUG: print "Recieving "+ str(BUFFER_SIZE) + "bytes from socket...\n"
+            client_request = conn_client.recv(BUFFER_SIZE) # Recieve data from user
+            if DEBUG: print "Creating new thread..."
+            start_new_thread(conn_client_thread, (conn_client, client_request, client_request)) #Start new thread to handle request from user
         except KeyboardInterrupt:
-            conn_client.close()
-            s.close()
-            print "Closing proxy while trying to accept..."
+            print "[*] Closing proxy while trying to accept..."
             sys.exit(1)
-    s.close()
+    conn_server.close()
 
-def conn_thread(conn_client, data, addr):
+def conn_client_thread(conn_client, client_request, addr):
     try:
-        print str(data)
-        webserver = get_url_from_req(data)
-        print webserver
+        if DEBUG: print "Handling request...:"
+        print client_request
+        host = get_host(client_request)
+        print "Connecting to Host: " + host
 
-        proxy_server(webserver, 80, conn_client, addr, data)
+        if DEBUG: print "Creating proxy client socket\n"
+        proxy_server(host, 80, conn_client, addr, client_request)
     except Exception, e:
-        print "Something went wrong when reading:\n"
+        print "[*] Something went wrong when reading:\n"
         print e.message
         sys.exit()
 
-def proxy_server(webserver, port, conn_client, addr, data):
+def proxy_server(host, port, conn_client, addr, client_request):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((webserver, port))
-        s.send(data)
+        conn_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print "Connecting to:\n"
+        print host
+        print port
 
-        #Look for answer from webserver
+        print "[*] I got here!"
+        conn_server.connect((host, port)) #Crashes....
+        print "[*] But not here!..."
+
+        conn_server.send(client_request)
+
+        #Look for answer from host
         while 1:
-            answer = s.recv(BUFFER_SIZE)
+            if DEBUG: print "Recieving answer from: " + host + "...\n"
+            answer = conn_server.recv(BUFFER_SIZE)
             if (len(answer)>0): #If any
+                if DEBUG: print "Sending answer to user...\n"
                 conn_client.send(answer) #send it to user
             else: # No return message (left)
                 break
-        s.close() # Close server socket
+        conn_server.close() # Close server socket
         conn_client.close() #Close client socket, no more data
     except socket.error, (value, message):
-        print "Socket exited when trying to send:"
+        print "[*] Socket exited when trying to send because"
         print message
         print value
-        s.close()
+        if DEBUG: print "[*] Closing all sockets...\n"
+        conn_server.close()
         conn_client.close()
+        if DEBUG: print "[*] Killing thread badly...\n"
         sys.exit(1)
+    if DEBUG: print "[*] Killing thread in a good way...\n"
     sys.exit(0)
 
-def get_url_from_req(data):
+def get_host(data):
     second_line = data.split('\n')[1]
     url = second_line.split(' ')[1]
     return url
